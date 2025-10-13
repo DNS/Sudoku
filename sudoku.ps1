d9233c96bb9c52a56dc4cda57d84de5871a553e3
DNS <37093+DNS@users.noreply.github.com>
2025-10-13 07:18:19 +0700

    -

--- sudoku.ps1
+++ sudoku.ps1
@@ -1,1 +1,31 @@
 ﻿
+
+
+$a = @(
+	@('5','3','.'),
+	@('6','.','.'),
+	@('.','9','8')
+)
+
+
+function Print-Board {
+	0 .. $a.Length | %{ $a[$_] -join '' }
+# 	for ($i=0; $i-lt3; $i++ ) {
+# 		for ($j=0; $j-lt3; $j++ ) {
+# 			
+# 		}
+# 	}
+}
+
+$a
+
+<#
+function Try3x3 {
+	$target_number
+	for ($i=0; $i -lt 3; $i++) {
+		for ($j=0; $j -lt 3; $j++) {
+			$b[$i,$j]
+		}
+	}
+}
+#>
