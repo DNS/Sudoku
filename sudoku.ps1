326ed9fb79b2578dbc5f2361e76e14d888cc2509
DNS <37093+DNS@users.noreply.github.com>
2025-10-14 03:19:42 +0700

    -

--- sudoku.ps1
+++ sudoku.ps1
@@ -9,7 +9,7 @@ $b = @(
 	123
 	231
 	312
-#>
+
 
 
 $b = @(
@@ -17,20 +17,26 @@ $b = @(
     @('.', '.', '.'),
     @('.', '.', 2)
 )
+#>
 
+$b = @(
+    @(1, 2, 3),
+    @(2, 3, 1),
+    @(3, 1, 2)
+)
 
 function Pick-Num {
 	param ($row, $col)
 	
 	$nums = [System.Collections.ArrayList]::new(1..3)
 	
-	for ($r = 0; $r -lt; 3; $r++) {
+	for ($r = 0; $r -lt 3; $r++) {
 		if ($b[$r][$col] -ne '.') {
 			$nums.Remove( $b[$r][$col] )
 		}
 	}
 	
-	for ($c = 0; $c -lt; 3; $c++) {
+	for ($c = 0; $c -lt 3; $c++) {
 		if ($b[$row][$c] -ne '.') {
 			$nums.Remove( $b[$row][$c] )
 		}
@@ -42,27 +48,30 @@ function Pick-Num {
 function Is-Valid {
 	param ($row, $col)
 	
-	$q = 1+2+3
+	$q = $(1..3 | Measure-Object -Sum).Sum
+
 	$t1 = 0
-	for ($r = 0; $r -lt; 3; $r++) {
-		$b[r] | %{ $t1 += $_ }
-	}
+	$b[$row] | %{ $t1 += $_ }	
+	
 	if ($t1 -ne $q) { return $false }
 	
 	$t2 = 0
-	for ($c = 0; $c -lt; 3; $c++) {
-		$t2 += $b[$row][$c]
+	for ($r = 0; $r -lt 3; $r++) {
+		$t2 += $b[$r][$col]
 	}
+	
 	if ($t2 -ne $q) { return $false }
 	
 	return $true
 }
 
-function  {
 
-}
 
 function Sudoku-Solver {
 
 }
+
+Is-Valid 2 2
+
+
 
